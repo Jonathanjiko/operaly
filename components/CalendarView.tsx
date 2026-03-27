98062727ce774491ae4ddca3cc5f7b66e24f82f5
Jonathanjiko <127270580+Jonathanjiko@users.noreply.github.com>
2026-03-26 20:39:46 -0500

    Create CalendarView.tsx

---
+++ components/CalendarView.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar"
+import { format, parse, startOfWeek, getDay } from "date-fns"
+import es from "date-fns/locale/es"
+
+const locales = { es }
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+export default function CalendarView({ events }: any) {
+  return (
+    <div className="h-[700px] bg-white rounded-2xl p-4">
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        culture="es"
+      />
+    </div>
+  )
+}
