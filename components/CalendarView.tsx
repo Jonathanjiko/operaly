f3a82733943ce6b2df2b5d5c893fd297ee0617ee
Jonathanjiko <127270580+Jonathanjiko@users.noreply.github.com>
2026-03-26 21:46:47 -0500

    Update CalendarView.tsx

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -1,28 +1,45 @@
 "use client"
+
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import { Calendar, dateFnsLocalizer } from "react-big-calendar"
 import { format, parse, startOfWeek, getDay } from "date-fns"
 import es from "date-fns/locale/es"
 
-const locales = { es }
+const locales = {
+  es,
+}
 
 const localizer = dateFnsLocalizer({
   format,
   parse,
-  startOfWeek,
+  startOfWeek: () => startOfWeek(new Date(), { weekStartsOn: 1 }),
   getDay,
   locales,
 })
 
-export default function CalendarView({ events }: any) {
+type CalendarEvent = {
+  title: string
+  start: Date
+  end: Date
+}
+
+export default function CalendarView({
+  events,
+}: {
+  events: CalendarEvent[]
+}) {
   return (
-    <div className="h-[700px] bg-white rounded-2xl p-4">
+    <div className="h-[760px] bg-white rounded-2xl p-4 shadow">
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         culture="es"
+        views={["month", "week", "day", "agenda"]}
+        defaultView="month"
+        popup
+        style={{ height: "100%" }}
       />
     </div>
   )
