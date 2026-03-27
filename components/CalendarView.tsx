9f735d38b1620c0e19e80a2d163d7a27d2f2e2e2
Jonathanjiko <127270580+Jonathanjiko@users.noreply.github.com>
2026-03-26 21:46:10 -0500

    Update CalendarView.tsx

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import "react-big-calendar/lib/css/react-big-calendar.css"
 import { Calendar, dateFnsLocalizer } from "react-big-calendar"
 import { format, parse, startOfWeek, getDay } from "date-fns"
 import es from "date-fns/locale/es"
